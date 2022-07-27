78604864f59856435bf7054de7811e1afe8d365a
Fernando Gomes <fernandogomesbernardo@gmail.com>
2022-07-26 22:34:33 -0700

    style: criacao do menu de navegacao

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -1,1 +1,13 @@
 <h3>Principal (view) </h3>
+
+<ul>
+    <li>
+        <a href="/">Principal</a>
+    </li>
+    <li>
+        <a href="/sobre">Sobre Nos</a>
+    </li>
+    <li>
+        <a href="/contacto">Contacto</a>
+    </li>
+</ul>
